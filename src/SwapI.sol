482bd2a9828ff02dc7f3803068156cc0f2a2e6b0
Lev Livnev <lev@liv.nev.org.uk>
2019-06-26 15:36:47 +0100

    SwapI.sol: fix compatibility interface arguments

--- src/SwapI.sol
+++ src/SwapI.sol
@@ -2,16 +2,21 @@ interface SwapSimple {
   function swapSimple(
     uint256 takeId,
     uint256 killId,
-    address makerWallet,
-    uint256 makerParam,
-    address makerToken,
-    address takerWallet,
-    uint256 takerParam,
-    address takerToken,
     uint256 expiry,
+    address makerWallet,
+    address makerToken,
+    uint256 makerParam,
+    address takerWallet,
+    address takerToken,
+    uint256 takerParam,
+    address affiliateWallet,
+    address affiliateToken,
+    uint256 affiliateParam,
+    address signer,
     bytes32 r,
     bytes32 s,
     uint8 v
+    bytes1 version
   )
     external payable;
   function cancel(uint256[] calldata ids) external;
